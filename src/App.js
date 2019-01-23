f74ee254cb60a7a13ac970127981cde1445ff6c2
Dusty Wood Saunders <dustywsaunders@gmail.com>
2019-01-23 11:34:16 +0100

    pre-routing functionality done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
 import './App.css';
-import DogsList from './components/DogList';
+import DogsListContainer from './components/DogsListContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        {/* <header className="App-header">
-          
-        </header> */}
         <main>
-          <DogsList />
+          <DogsListContainer />
         </main>
       </div>
     );
