18d9845f88a0f81caed095d5d5dd6ba5e8206fad
Dusty Wood Saunders <dustywsaunders@gmail.com>
2019-01-23 12:22:43 +0100

    links implimented

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React, { Component } from 'react';
 import './App.css';
 import DogsListContainer from './components/DogsListContainer';
+import DogBreedImages from './components/DogBreedImages';
+import { Route } from 'react-router-dom';
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <main>
-          <DogsListContainer />
+          <Route exact path="/" component={DogsListContainer} />
+          <Route path="/dog-breeds/:breed" component={DogBreedImages} />
         </main>
       </div>
     );
---
+++ src/components/DogBreedImages.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+
+export default function DogBreedImages (props) {
+  return (
+    <div className="dog-breed-images">
+      <h1>Dog Breed Images</h1>
+
+      This page will show images of the { props.match.params.breed } breed.<br />
+
+      <Link to = '/'>Go back to the Index</Link>
+    </div>
+  )
+}
