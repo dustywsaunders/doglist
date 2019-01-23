e09266ef81437d86818a289480ba6f60155ca7cc
Dusty Wood Saunders <dustywsaunders@gmail.com>
2019-01-23 10:58:05 +0100

    dog breeds added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
+import DogsList from './components/DogList';
 
 class App extends Component {
   render() {
@@ -9,7 +10,7 @@ class App extends Component {
           
         </header> */}
         <main>
-
+          <DogsList />
         </main>
       </div>
     );
